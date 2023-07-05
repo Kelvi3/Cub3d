980cf14ce93448f367055ca13117648fd47494fd
Luis Laens <lulaens@3C5.42angouleme.fr>
2023-07-05 14:14:29 +0200

    color hex ceiling and floor

--- raycasting/calculate_raycasting.c
+++ raycasting/calculate_raycasting.c
@@ -9,42 +9,48 @@ void	calculate_pos_and_dir(t_cast *cast, t_map img)
 	cast->mapY = (int)img.posY;
 }
 
-void	calculate_step_and_initial_sidedist(t_cast *cast, t_map img)
+void	step_and_init_sidedist(t_cast *cast)
 {
 	if (cast->rayDirX == 0.0)
 		cast->deltaDistX = 1e30;
 	else
 		cast->deltaDistX = fabs(1.0 / cast->rayDirX);
-
 	if (cast->rayDirY == 0.0)
 		cast->deltaDistY = 1e30;
 	else
 		cast->deltaDistY = fabs(1.0 / cast->rayDirY);
+}
 
+void	calculate_step_and_initial_sidedist(t_cast *cast, t_map img)
+{
+	step_and_init_sidedist(cast);
 	if (cast->rayDirX < 0.0)
 	{
 		cast->stepX = -1;
-		cast->sideDistX = (double)(img.posX - (double)cast->mapX) * cast->deltaDistX;
+		cast->sideDistX = ((double)(img.posX - (double)cast->mapX)) \
+					* cast->deltaDistX;
 	}
 	else
 	{
 		cast->stepX = 1;
-		cast->sideDistX = ((double)cast->mapX + 1.0 - img.posX) * cast->deltaDistX;
+		cast->sideDistX = ((double)cast->mapX + 1.0 - img.posX) \
+						* cast->deltaDistX;
 	}
 	if (cast->rayDirY < 0.0)
 	{
 		cast->stepY = -1;
-	cast->sideDistY = (img.posY - (double)cast->mapY) * cast->deltaDistY;
+		cast->sideDistY = (img.posY - (double)cast->mapY) \
+						* cast->deltaDistY;
 	}
 	else
 	{
 		cast->stepY = 1;
-		cast->sideDistY = ((double)cast->mapY + 1.0 - img.posY) * cast->deltaDistY;
+		cast->sideDistY = ((double)cast->mapY + 1.0 - img.posY) \
+						* cast->deltaDistY;
 	}
 }
 
-
-/* calcule le pixel le plus bas et le plus haut pour remplir la bande actuell */
+/* calcule le pixel le plus bas et le plus haut pour remplir la bande actuelle */
 
 void	calculate_lowest_and_highest_pixel(t_cast *cast)
 {
--- raycasting/raycasting.c
+++ raycasting/raycasting.c
@@ -6,58 +6,48 @@ void	my_mlx_pixel_put(t_map *data, int x, int y, int color)
 		((int *)data->addr)[y * 1280 + x] = color;
 }
 
-void	img_pix_put(t_map img, t_cast cast, int color)
+void	img_pix_put(t_map img, int x, int y, int color)
 {
 	char    *pixel;
 
-	pixel = img.addr + (cast.y * 1280 + cast.x * (img.bits_per_pixel / 8));
+	pixel = NULL;
+	color = (y * 1280 + x * (img.bits_per_pixel / 8));
 	*(int *)pixel = color;
 }
-void	floor_wall_ceiling(t_map img, t_cast cast)
+void	floor_wall_ceiling(t_map img, t_cast cast, t_data map)
 {
-	int	x = 0;
-	int y = 0;
+	//int	x = 0;
+//	int y = 0;
+	int	ceiling;
+	int	floor;
 	cast.y = 0;
-   while (x < 1280)
-    {
-        y = 0;
-        while (y < 720)
-        {
-			img_pix_put(img, cast, cast.color);
-          //  ((int *)img.addr)[y * 1280 + x] = color;
-            y++;
-        }
-        x++;
-    }
+
 	while (cast.y < cast.drawStart)
 	{
-		int	color2 = 0xFF000;
-		my_mlx_pixel_put(&img, cast.x, cast.y, color2);
-		//img_pix_put(img, cast, color2);
+		ceiling = (map.colorc[0] << 16) | (map.colorc[1] << 8) | map.colorc[2];
+		my_mlx_pixel_put(&img, cast.x, cast.y, ceiling);
 		cast.y++;
 	}
 	while (cast.y < cast.drawEnd)
 	{
-		//my_mlx_pixel_put(&img, x, y, color);
 		if (img.dirX == 1.0 && img.dirY == 0.0)
-			cast.color = 0x05ff00; // SUD
+			cast.color = 0x3272A3; // SUD
 		if (img.dirX == 0.0 && img.dirY == 1.0)
-			cast.color = 0x05ff00; // NORD
+			cast.color = 0x3200A3; // NORD
 		if (img.dirX == -1.0 && img.dirY == 0.0)
-			cast.color = 0x05ff00; // WEST
+			cast.color = 0x3200A3; // WEST
 		if (img.dirX == 0.0 && img.dirY == -1.0)
-			cast.color = 0x00FFFF; // EST
+			cast.color = 0x140004; // EST
 		//cast.color = (cast.y * 1280 + cast.x * (img.bits_per_pixel / 8));
 		//*(int *)img.addr = cast.color;
-		//color = 0x00FFFF;
-		//img_pix_put(img, cast, cast.color);
+	//	img_pix_put(img, cast.x, cast.y, cast.color);
 		my_mlx_pixel_put(&img, cast.x, cast.y, cast.color);
 		cast.y++;
 	}
 	while (cast.y < 720)
 	{
-		int color1 = 0x000FF;
-		my_mlx_pixel_put(&img, cast.x, cast.y, color1);
+		floor = (map.colorf[0] << 16) | (map.colorf[1] << 8) | map.colorf[2];
+		my_mlx_pixel_put(&img, cast.x, cast.y, floor);
 		cast.y++;
 	}
 }
@@ -85,6 +75,34 @@ t_cast	dda(t_cast cast, t_map img)
 	return (cast);
 }
 
+void	raycasting_loop(t_map img, t_cast *cast, t_data map)
+{
+	while (cast->x < cast->w)
+	{
+		calculate_pos_and_dir(cast, img);
+		calculate_step_and_initial_sidedist(cast, img);
+		*cast = dda(*cast, img);
+		if (cast->side == 0)
+			cast->perpWallDist = (cast->sideDistX - cast->deltaDistX);
+		else
+			cast->perpWallDist = (cast->sideDistY - cast->deltaDistY);
+		cast->lineHeight = (int)((double)cast->h / cast->perpWallDist);
+		calculate_lowest_and_highest_pixel(cast);
+		if (cast->side == 0)
+			cast->wallX = img.posY + cast->perpWallDist * cast->rayDirY;
+		else
+			cast->wallX = img.posX + cast->perpWallDist * cast->rayDirX;
+		cast->wallX -= cast->wallX;
+		cast->texX = (int)(cast->wallX * (double)cast->texWidth);
+		if (cast->side == 0 && cast->rayDirX > 0.0)
+			cast->texX = cast->texWidth - cast->texX - 1;
+		if (cast->side == 1 && cast->rayDirY < 0.0)
+			cast->texX = cast->texWidth - cast->texX - 1;
+		floor_wall_ceiling(img, *cast, map);
+		cast->x++;
+	}
+}
+
 t_map raycasting(t_data map, t_map img, t_cast cast)
 {
 	cast.texWidth = 64;
@@ -93,55 +111,36 @@ t_map raycasting(t_data map, t_map img, t_cast cast)
 	cast.w = 1280;
 	cast.h = 720;
 	img.map = map;
-//	int	we = 64;
-//	int	he = 64;
+	//int	we = 64;
+	//int	he = 64;
 	img.img = mlx_new_image(img.mlx, 1280, 720);
-//	img.img = mlx_xpm_file_to_image(img.mlx, "textures/wall.xpm", &he, &we);
+	//img.img = mlx_xpm_file_to_image(img.mlx, "textures/wall.xpm", &he, &we);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	printf("endian = %d\n", img.endian);
 	printf("bpp = %d\n", img.bits_per_pixel);
-	if (img.dirX == 1.0 && img.dirY == 0.0)
-		cast.color = 0x808000; // SUD
-	if (img.dirX == 0.0 && img.dirY == 1.0)
-		cast.color = 0xC0C0C0; // NORD
-	if (img.dirX == -1.0 && img.dirY == 0.0)
-		cast.color = 0xFFFF00; // WEST
-	if (img.dirX == 0.0 && img.dirY == -1.0)
-		cast.color = 0x00FFFF; // EST
-	cast.color = 0x000000; // SUD
-   /*while (x < 1280)
-    {
-        y = 0;
-        while (y < 720)
-        {
-            ((int *)img.addr)[y * 1280 + x] = color;
-            y++;
-        }
-        x++;
-    }*/
-	while (cast.x < cast.w)
+	if (img.dirX <= 1.0 && img.dirY >= 0.0)
+		cast.color = 0x3272A3; // SUD
+	if (img.dirX >= 0.0 && img.dirY <= 1.0)
+		cast.color = 0x3200A3; // NORD
+	if (img.dirX >= -1.0 && img.dirY <= 0.0)
+		cast.color = 0x3200A3; // WEST
+	if (img.dirX <= 0.0 && img.dirY >= -1.0)
+		cast.color = 0x140004; // EST
+	printf("dirX = %f\n", img.dirX);
+	printf("dirY = %f\n", img.dirY);
+	while (cast.x < 1280)
 	{
-		calculate_pos_and_dir(&cast, img);
-		calculate_step_and_initial_sidedist(&cast, img);
-		cast = dda(cast, img);
-		if (cast.side == 0)
-			cast.perpWallDist = (cast.sideDistX - cast.deltaDistX);
-		else
-			cast.perpWallDist = (cast.sideDistY - cast.deltaDistY);
-		cast.lineHeight = (int)((double)cast.h / cast.perpWallDist);
-		calculate_lowest_and_highest_pixel(&cast);
-		if (cast.side == 0)
-			cast.wallX = img.posY + cast.perpWallDist * cast.rayDirY;
-		else
-			cast.wallX = img.posX + cast.perpWallDist * cast.rayDirX;
-		cast.wallX -= cast.wallX;
-		cast.texX = (int)(cast.wallX * (double)cast.texWidth);
-		if (cast.side == 0 && cast.rayDirX > 0.0)
-			cast.texX = cast.texWidth - cast.texX - 1;
-		if (cast.side == 1 && cast.rayDirY < 0.0)
-			cast.texX = cast.texWidth - cast.texX - 1;
-		floor_wall_ceiling(img, cast);
+		cast.y = 0;
+		while (cast.y < 720)
+		{
+			((int *)img.addr)[cast.y * 1280 + cast.x] = cast.color;
+			cast.y++;
+		}
 		cast.x++;
 	}
+	cast.y = 0;
+	cast.x = 0;
+	raycasting_loop(img, &cast, map);
 	mlx_put_image_to_window(img.mlx, img.mlx_win, img.img, 0, 0);
 	return (img);
 }
