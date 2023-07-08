2699ea1fbf629481ba62a21173f52f8702a65e61
Theo Cazenave-coupet <tcazenav@3C6.42angouleme.fr>
2023-07-08 13:57:47 +0200

    norme

--- main.c
+++ main.c
@@ -1,30 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tcazenav <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/08 13:46:22 by tcazenav          #+#    #+#             */
+/*   Updated: 2023/07/08 13:56:55 by tcazenav         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "MLX42/include/MLX42/MLX42.h"
 #include "cub3D.h"
 
-void print_all(t_data map)
+t_map	player_view(t_map img, char c)
 {
-	printf("NO = %s\n", map.no);
-	printf("SO = %s\n", map.so);
-	printf("WE = %s\n", map.we);
-	printf("EA = %s\n", map.ea);
-
-	printf("F = %s\n", map.color_f);
-	printf("C = %s\n", map.color_c);
-	int i = 0;
-	while (map.map[i])
+	if (c == 'S')
 	{
-		printf("%s", map.map[i]);
-		i++;
+		img.dirX = 1.0;
+		img.planeY = 0.66;
 	}
+	if (c == 'N')
+	{
+		img.dirX = -1.0;
+		img.planeY = -0.66;
+	}
+	if (c == 'W')
+	{
+		img.dirY = 1.0;
+		img.planeX = -0.66;
+	}
+	if (c == 'E')
+	{
+		img.dirY = -1.0;
+		img.planeX = 0.66;
+	}
+	return (img);
 }
 
-t_map get_pos_player(t_data map, t_map img)
+t_map	get_pos_player(t_data map, t_map img)
 {
-	int i;
-	int j;
+	int	i;
+	int	j;
 
 	i = 0;
-	j = 0;
 	img.dirX = 0.0;
 	img.dirY = 0.0;
 	img.planeX = 0.0;
@@ -39,26 +58,7 @@ t_map get_pos_player(t_data map, t_map img)
 			{
 				img.posX = (double)i + 0.5;
 				img.posY = (double)j + 0.5;
-				if (map.map[i][j] == 'S')
-				{
-					img.dirX = 1.0;
-					img.planeY = 0.66;
-				}
-				if (map.map[i][j] == 'N')
-				{
-					img.dirX = -1.0;
-					img.planeY = -0.66;
-				}
-				if (map.map[i][j] == 'W')
-				{
-					img.dirY = 1.0;
-					img.planeX = -0.66;
-				}
-				if (map.map[i][j] == 'E')
-				{
-					img.dirY = -1.0;
-					img.planeX = 0.66;
-				}
+				img = player_view(img, map.map[i][j]);
 			}
 			j++;
 		}
@@ -67,7 +67,19 @@ t_map get_pos_player(t_data map, t_map img)
 	return (img);
 }
 
-int main(int argc, char **argv)
+t_map	load_image(t_data map, t_map img)
+{
+	img = get_pos_player(map, img);
+	img.texture_n = mlx_load_png(map.no);
+	img.texture_s = mlx_load_png(map.so);
+	img.texture_e = mlx_load_png(map.ea);
+	img.texture_w = mlx_load_png(map.we);
+	img.image = mlx_new_image(img.mlx, 1280, 720);
+	img = raycasting(map, img, img.cast);
+	return (img);
+}
+
+int	main(int argc, char **argv)
 {
 	t_data	map;
 	t_map	img;
@@ -82,29 +94,14 @@ int main(int argc, char **argv)
 	add_all_cube(argv, &map);
 	if (parse(&map) == 1)
 	{
-		print_all(map);
 		free_all(&map);
 		return (1);
 	}
-	print_all(map);
 	img.mlx = mlx_init(WIDTH, HEIGHT, "CUB3D", true);
 	if (!img.mlx)
 		return (2);
-	// TODO : create function vecteur for get dirX dirY
 	img.map = map;
-	img = get_pos_player(map, img);
-	img.texture_n = mlx_load_png("textures/psg.png");
-	img.texture_s = mlx_load_png("textures/marseille.png");
-	img.texture_e = mlx_load_png("textures/lyon.png");
-	img.texture_w = mlx_load_png("textures/bordeaux.png");
-	/*
-	img.texture_n = mlx_load_png(map.no);
-	img.texture_s = mlx_load_png(map.so);
-	img.texture_e = mlx_load_png(map.ea);
-	img.texture_w = mlx_load_png(map.we);
-	*/
-	img.image = mlx_new_image(img.mlx, 1280, 720);
-	img = raycasting(map, img, img.cast);
+	img = load_image(map, img);
 	mlx_loop_hook(img.mlx, moov_player, &img);
 	mlx_loop(img.mlx);
 	free_all(&map);
